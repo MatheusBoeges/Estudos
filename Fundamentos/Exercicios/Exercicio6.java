7298e94a7a05408f725601d3cdc27bb2ba924adc
Matheus <matheus.s.bcorrea@gamil.com>
2023-11-07 22:03:33 -0300

    c1 - Corrigindo o exercício 6

--- Fundamentos/Exercicios/Exercicio6.java
+++ Fundamentos/Exercicios/Exercicio6.java
@@ -1,5 +1,7 @@
 package Fundamentos.Exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio6 {
 	
 	public static void main(String[] args) {
@@ -7,16 +9,34 @@ public class Exercicio6 {
 		/* Criar um programa que resolve equações do segundo grau (ax2 + bx + c = 0) utilizando a fórmula de Bhaskara.
 		 * Use como exemplo a = 1, b = 12 e c = -13. Encontre o delta */
 		
-		double a = 1;
-		double b = 12;
-		double c = -13;
+		Scanner entrada = new Scanner(System.in);
 		
-		double delta = Math.pow(b, 2) - 4 * a *c;
+		System.out.println("Equação: ax² + bx + c = 0");
 		
-		double raiz1 = (- b + Math.sqrt(delta)) / 2 * a;
-		double raiz2 = (- b - Math.sqrt(delta)) / 2 * a;
+		System.out.println("\nDigite o valor de A: ");
+		int a = entrada.nextInt();
+
+		System.out.println("\nDigite o valor de B: ");
+		int b = entrada.nextInt();
+
+		System.out.println("\nDigite o valor de C: ");
+		int c = entrada.nextInt();
+
+		int delta = (b * b) - (4 * a * c);
+
+		System.out.printf("\nSua equação é: %dx² + %dx + %d = 0", a, b, c);
 		
-		System.out.println("As raízes da equação são: " + raiz1 + " e " + raiz2);
+		System.out.println("\nO delta é: " + delta);
+
+		double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+
+		System.out.printf("O x1 da equação é: %.2f", x1);
+
+		double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+		System.out.printf("\nO x2 da equação é: %.2f" ,x2);
+		
+		entrada.close();
 		
 	}
 
